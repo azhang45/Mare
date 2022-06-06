90b13a461ce71433ca5f7c2a1ece4395f5734305
ComputerScienceSky <78038241+ComputerScienceSky@users.noreply.github.com>
2022-06-06 10:20:44 -0400

    Update imageChange.js

--- imageChange.js
+++ imageChange.js
@@ -1,4 +1,4 @@
-const img = document.getElementById('eeveelutions');
+const img = document.getElementById('');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
